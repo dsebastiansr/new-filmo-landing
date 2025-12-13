7088770cb42cadb577a274327eaca9476230a08c
David Sanchez <dsebastiansr24@gmail.com>
2025-12-12 23:15:47 -0500

    [ADD] Ws link

--- frontend/app/components/ui/ContactUsButton.tsx
+++ frontend/app/components/ui/ContactUsButton.tsx
@@ -5,7 +5,7 @@ export default function ContactUsButton() {
   return (
     <Link
       target="_blank"
-      href=""
+      href="https://wa.me/+593983903842"
       className="flex justify-end gap-2 bg-filmo-yellow-100 duration-300 hover:scale-105   text-filmo-black-100 rounded-full px-5 py-3 font-bold uppercase max-md:px-3 max-md:py-2 max-md:text-sm"
     >
       <Image src="/svg/ws.svg" className="h-6 w-6" alt="" width={0} height={0} />
